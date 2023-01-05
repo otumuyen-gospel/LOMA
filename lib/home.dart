9115c6cd4c27da6d9812b49f48c2f56bbe976d53
Your Name <otumsgosepel@gmail.com>
2023-01-05 23:37:04 +0000

    just began the design of the product and home page

--- lib/home.dart
+++ lib/home.dart
@@ -11,21 +11,38 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
+           backgroundColor: Colors.white,
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
-          title: const Text("LOMA"),
-          leading: null,
-          actions: const [],
+          title: const Text("LOMA", style: TextStyle(color: Colors.blue,fontFamily: 'verdana'),),
+          leading: Builder(builder: (context){
+            return IconButton(
+                onPressed: (){
+                  Scaffold.of(context).openDrawer();
+                },
+                icon: const Icon(Icons.menu, color: Colors.blue,),
+            );
+          }),
+          actions: [
+            TextButton(
+                onPressed: (){},
+                child: const Text("Stores", style: TextStyle(color: Colors.blue,fontFamily: 'verdana'),),
+            ),
+            OutlinedButton(
+                onPressed:  (){},
+                style:  ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.blue),),
+                child: const Text("Account", style: TextStyle(color:Colors.white,fontFamily: 'verdana'),),
+            ),
+          ],
         ),
-        drawer: null,
-        body: const Center(
-          child: Text("LOCAL MARKET APP"),
+        drawer: const Drawer(
+
         ),
-      floatingActionButton: const FloatingActionButton(
-        child: null,
-        onPressed: null,
+        body: null,
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.search),
+        onPressed: (){},
       ),
       bottomNavigationBar: null,
     );
